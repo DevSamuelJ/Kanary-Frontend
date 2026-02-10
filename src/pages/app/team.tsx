9bbfa05a2b09c966bda70467a2615927e9d5ad18
diegodasp88 <diego.gps@gmail.com>
2026-02-09 22:33:39 -0300

    Future - aplicando as rotas na telas do app.

--- src/pages/app/team.tsx
+++ src/pages/app/team.tsx
@@ -1,5 +1,11 @@
 export const Equipe = () => {
     return(
-        <div>equipe</div>
+        <div id="container" className="min-w-full h-full flex flex-col px-4 md:px-7 pb-5 md:pt-5 min-h-0">    
+            <div id="title" className="flex items-center justify-between p-5 w-full">
+                <h1 className="text-3xl font-medium">Equipe</h1>
+            </div>
+            <div id="content" className="bg-[#0F0B15] w-full flex flex-col flex-1 rounded-3xl py-5 px-2 min-h-0">              
+            </div>
+        </div>
     )
 }
