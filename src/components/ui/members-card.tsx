7a95007aacd37f9c226bee04344cd05fe7b9e54c
diegodasp88 <diego.gps@gmail.com>
2026-04-06 20:37:38 -0300

    Future - Finalizando a página de Equipe

--- src/components/ui/members-card.tsx
+++ src/components/ui/members-card.tsx
@@ -1,4 +1,5 @@
 import { BsTrash } from "react-icons/bs";
+import { FaRegEdit } from "react-icons/fa";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
 type MembersCardProps = {
@@ -8,12 +9,15 @@ type MembersCardProps = {
 }
 
 export const MembersCard = ({name, role, color}: MembersCardProps) => (
-    <div className="flex items-center py-1">
+    <div className="flex items-center py-1 px-3 rounded-3xl mb-2 border-1 border-white/10">
         <p className="text-6xl cursor-pointer pr-2" style={{ color: color }}><IoPersonCircleOutline/></p>
         <div className="w-full">
             <p>{name}</p>
             <p className="text-[12px] font-light">{role}</p>
         </div>
-        <p className="cursor-pointer"><BsTrash/></p>
+        <div className="flex gap-2">
+            <p className="cursor-pointer"><FaRegEdit/></p>
+            <p className="cursor-pointer"><BsTrash/></p>
+        </div>
     </div>
 );
--- src/pages/app/team.tsx
+++ src/pages/app/team.tsx
@@ -1,18 +1,19 @@
 import { MdPersonAdd } from "react-icons/md"
 import { CustomBtn2 } from "../../components/ui/custom-btn-2"
 import { GrAddCircle } from "react-icons/gr"
-import { CgRename } from "react-icons/cg"
+import { CgCheckO, CgList, CgMail, CgRename } from "react-icons/cg"
 import { CustomInputBox } from "../../components/ui/custom-input-box"
 import { listMembers } from "../../components/data/list-members"
 import { MembersCard } from "../../components/ui/members-card"
+import { BsTrash } from "react-icons/bs"
 
 export const Equipe = () => {
 
     const listRoles = [
         { "id": 1, "role": "Designer" },
-        { "id": 2, "role": "Analist" },
-        { "id": 3, "role": "Developer" },
-        { "id": 4, "role": "Engineer" },
+        { "id": 2, "role": "Analista" },
+        { "id": 3, "role": "Desenvolvedor" },
+        { "id": 4, "role": "Engenheiro" },
     ]
 
     return (
@@ -20,58 +21,111 @@ export const Equipe = () => {
             <div id="title" className="flex items-center justify-between p-5 w-full">
                 <h1 className="text-3xl font-medium">Equipe</h1>
             </div>
-            <div id="content" className="bg-[#0F0B15] w-full flex flex-col flex-1 rounded-3xl py-5 px-5 min-h-0">   
+            <div id="content" className="bg-[#0F0B15] w-full flex flex-col flex-1 rounded-3xl py-5 px-5 min-h-0">
 
-                <section id="memberSection" className="grid grid-cols-1 lg:grid-cols-2 gap-5 flex-1 p-3">
-                    
-                    {/* Formulário de membros */}
-                    <div className="flex flex-col">
-                        <div className="flex w-full pl-3 items-center pb-3">
-                            <MdPersonAdd className="text-2xl" />
-                            <h2 className="p-2 pl-3 text-lg"> Adicionar membro </h2>
-                        </div>
-                        <form action="#" className="w-full max-w-130 pb-2">
-                            <CustomInputBox
-                                type="text"
-                                name="name"
-                                placeholder="Nome"
-                                icon={<CgRename className="text-white" />}
-                                className="bg-white/25 text-white"
+                <div id="content-scroll" className="overflow-y-auto simple-scroll">
+
+                    <section id="memberSection" className="grid grid-cols-1 lg:grid-cols-2 gap-5 flex-1 p-3">
+                        
+                        {/* Formulário de membros */}
+                        <div className="flex flex-col">
+                            <div className="flex w-full pl-3 items-center pb-3">
+                                <MdPersonAdd className="text-2xl" />
+                                <h2 className="p-2 pl-3 text-lg"> Adicionar membro </h2>
+                            </div>
+                            <form action="#" className="w-full max-w-130 pb-2">
+                                <CustomInputBox
+                                    type="text"
+                                    name="name"
+                                    placeholder="Nome"
+                                    icon={<CgRename className="text-white" />}
+                                    className="bg-white/25 text-white"
+                                />
+                                <CustomInputBox
+                                    type="email"
+                                    name="email"
+                                    placeholder="Email"
+                                    icon={<CgMail className="text-white" />}
+                                    className="bg-white/25 text-white"
+                                />
+                                <select 
+                                    name="projects" 
+                                    id="projects" 
+                                    defaultValue={""} 
+                                    className="bg-white/25 text-white border-white/30 border-2 rounded-lg pl-2 pt-0.5 pb-0.5 w-full hover:border-black/30 hover:border-2 cursor-pointer">
+                                        <option value="" disabled hidden className="bg-[#110c1a]"> Selecione uma função </option>
+                                        {listRoles.map((role) => 
+                                            <option key={role.id} value={role.id} className="bg-[#110c1a]">{role.role}</option>
+                                        )}
+                                </select>
+                            </form>
+                            <CustomBtn2 
+                                icon={<GrAddCircle className="text-lg" />}
+                                children="Adicionar" 
+                                className="w-50 mt-2"
                             />
-                            <select 
-                                name="projects" 
-                                id="projects" 
-                                defaultValue={""} 
-                                className="bg-white/25 text-white border-white/30 border-2 rounded-lg pl-2 pt-0.5 pb-0.5 w-full hover:border-black/30 hover:border-2 cursor-pointer">
-                                    <option value="" disabled hidden className="bg-[#110c1a]"> Selecione uma função </option>
-                                    {listRoles.map((role) => 
-                                        <option key={role.id} value={role.id} className="bg-[#110c1a]">{role.role}</option>
-                                    )}
-                            </select>
-                        </form>
-                        <CustomBtn2 
-                            icon={<GrAddCircle className="text-lg" />}
-                            children="Criar" 
-                            className="w-50 mt-2"
-                        />
-                    </div>
-
-                    <div className="py-6 px-4 md:px-8 rounded-xl border border-white/70 flex flex-col justify-between bg-[#110c1a]">
-                        <div className="flex justify-between pb-5">
-                            <h4>Membros de Time</h4>
                         </div>
-                        <div className="flex-1 overflow-y-auto pr-2">
-                            {listMembers.map(
-                                (member, index) => (<MembersCard key={index} name={member.name} role={member.role} color={member.color} />)
-                            )}
-                        </div>                  
-                    </div>  
-                    
-                </section>
 
-                <section id="roleSection" className="bg-red-400 flex flex-1">
+                        {/* Lista de Membros */}
+                        <div className="py-6 px-4 md:px-8 rounded-xl border border-white/70 flex flex-col justify-between bg-[#110c1a]">
+                            <div className="flex justify-between pb-5">
+                                <h4>Membros de Time</h4>
+                            </div>
+                            <div className="flex-1 overflow-y-auto pr-2">
+                                {listMembers.map(
+                                    (member, index) => (<MembersCard key={index} name={member.name} role={member.role} color={member.color} />)
+                                )}
+                            </div>                  
+                        </div>  
+                        
+                    </section>
 
-                </section>
+                    <section id="roleSection" className="grid grid-cols-1 lg:grid-cols-2 gap-5 flex-1 p-3 pt-5">
+                        
+                        {/* Formulário de funções */}
+                        <div className="flex flex-col">
+                            <div className="flex w-full pl-3 items-center pb-3">
+                                <MdPersonAdd className="text-2xl" />
+                                <h2 className="p-2 pl-3 text-lg"> Adicionar função </h2>
+                            </div>
+                            <form action="#" className="w-full max-w-130 pb-2">
+                                <CustomInputBox
+                                    type="text"
+                                    name="role"
+                                    placeholder="Função"
+                                    icon={<CgList className="text-white" />}
+                                    className="bg-white/25 text-white"
+                                />
+                            </form>
+                            <CustomBtn2 
+                                icon={<GrAddCircle className="text-lg" />}
+                                children="Adicionar" 
+                                className="w-50"
+                            />
+                        </div>
+
+                        {/* Lista de Funções */}
+                        <div className="py-6 px-4 md:px-8 rounded-xl border border-white/70 flex flex-col justify-between bg-[#110c1a]">
+                            <div className="flex justify-between pb-5">
+                                <h4>Lista de Funções</h4>
+                            </div>
+                            <div className="flex-1 overflow-y-auto pr-2">
+                                {listRoles.map(
+                                    (role) => (
+                                        <div key={role.id} className="flex items-center justify-between py-2 px-4 mb-2 rounded-2xl border-1 border-white/10">
+                                            <div className="flex items-center gap-2">
+                                                <CgCheckO/>
+                                                <p>{role.role}</p>    
+                                            </div>
+                                            <BsTrash className="cursor-pointer"/>
+                                        </div>
+                                    )
+                                )}
+                            </div>                  
+                        </div> 
+                    </section>
+
+                </div>
 
             </div>
         </div>
