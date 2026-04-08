f9006813c1a1f7c64ed09d8a318086130e6f7f37
devsamuelj <samuel701silva@gmail.com>
2026-04-07 22:46:35 -0300

    FIX: redirecionamento do router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
-import { Register } from "./pages/access/register";
+// import { Register } from "./pages/access/register";
+import { Register } from "./pages/access/Register";
 import { AppLayout } from "./pages/app/app-layout"
-import { Dashboard } from "./pages/app/dashboard";
+// import { Dashboard } from "./pages/app/dashboard";
+import { Dashboard } from "./pages/app/Dashboard";
 import { AccessLayout } from "./pages/access/access-layout";
 import { Recover } from "./pages/access/recover";
-import { Login } from "./pages/access/login";
+// import { Login } from "./pages/access/login";
+import { Login } from "./pages/access/Login";
 import { Board } from "./pages/app/board";
 import { Equipe } from "./pages/app/team";
 import { Projeto } from "./pages/app/project";
